834506c3ba9716a53ff40c90eb816ac03536c5b8
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-04-29 09:29:37 -0400

    started code

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,18 @@
 from capitals import states
 
-print(states)
+# print(states)
+
+
+class States:
+    def __init__(self):
+        self.states = states
+        for states in self.states:
+            self.name = states.name
+            self.capital = states.capital
+            print(self.name)
+
+
+States()
+
+# "name": "Alabama",
+# "capital": "Montgomery"
