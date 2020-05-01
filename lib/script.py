e6386b7ce6a0b257f8c8dc02ef7df6cabcd99687
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-01 16:24:26 -0400

    finished lab

--- lib/script.py
+++ lib/script.py
@@ -1,18 +1,35 @@
 from capitals import states
-
-# print(states)
-
-
-class States:
-    def __init__(self):
-        self.states = states
-        for states in self.states:
-            self.name = states.name
-            self.capital = states.capital
-            print(self.name)
+import random
+print("")
+print("-----------------------------------")
+print("Welcome to the State Capital Game!")
+print("-----------------------------------")
+print("")
+print("When prompted with a state, please give us the state's capital.")
+print("")
 
 
-States()
+def play():
+    random.shuffle(states)
+    score = 0
+    for i in range(len(states)):
+        guess = str(
+            input(f'{i+1}: What’s the capital of {(states[i]["name"])}? '))
+        if guess == str(states[i]["capital"]):
+            score += 1
+            print(
+                f' 🎉 Correct! +1 Point. Score is now {score}! 🎉')
+        else:
+            print(f"😕 Incorrect. Score is {score}!")
+    print("--------------------------------------------")
+    print(f"        Game Over. Final Score: {score}!")
+    print("     Would you like to play again? Yes/No      ")
+    play_again = str(input())
+    if play_again == "Yes":
+        play()
+    else:
+        print(" Goodbye! ")
+        exit()
 
-# "name": "Alabama",
-# "capital": "Montgomery"
+
+play()
